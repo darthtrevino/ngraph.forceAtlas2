8b45e9a919207ddd8506d983da719edc2b2903ab
Chris Trevino <darthtrevino@gmail.com>
2019-12-05 16:01:14 -0800

    add array readers

---
+++ src/core/Edges.ts
@@ -0,0 +1,51 @@
+export enum E {
+	source = 0,
+	target = 1,
+	weight = 2,
+}
+
+export const ppe = 3
+
+export class Edges {
+	private _ary: Uint32Array
+
+	public constructor(ary: Uint32Array) {
+		this._ary = ary
+	}
+
+	public set array(ary: Uint32Array) {
+		this._ary = ary
+	}
+
+	public get array() {
+		return this._ary
+	}
+
+	public get length() {
+		return this._ary.length
+	}
+
+	public source(index: number): number {
+		return this._ary[index + E.source]
+	}
+
+	public target(index: number) {
+		return this._ary[index + E.target]
+	}
+
+	public weight(index: number) {
+		return this._ary[index + E.weight]
+	}
+
+	public setSource(index: number, value: number) {
+		this._ary[index + E.source] = value
+	}
+
+	public setTarget(index: number, value: number) {
+		this._ary[index + E.target] = value
+	}
+
+	public setWeight(index: number, value: number) {
+		this._ary[index + E.weight] = value
+	}
+}
