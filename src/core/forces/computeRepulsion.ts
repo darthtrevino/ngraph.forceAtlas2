e4ab5e548a8d1bd9615a528caa0e081b818cf336
Chris Trevino <darthtrevino@gmail.com>
2019-12-13 14:20:07 -0800

    correct repulsion

--- src/core/forces/computeRepulsion.ts
+++ src/core/forces/computeRepulsion.ts
@@ -2,12 +2,13 @@ import { prepareBarnesHutOptimization } from './prepareBarnesHutOptimization'
 import { FA2Configuration } from '../../configuration'
 import { Nodes } from '../data_structures'
 import { computeRepulsionBarnesHut } from './computeRepulsionBarnesHut'
+import { computeRepulsionUnoptimized } from './computeRepulsionUnoptimized'
 
 export function computeRepulsion(nodes: Nodes, config: FA2Configuration) {
 	if (config.barnesHutOptimize) {
 		const regions = prepareBarnesHutOptimization(nodes)
 		computeRepulsionBarnesHut(nodes, regions, config)
 	} else {
-		computeRepulsion(nodes, config)
+		computeRepulsionUnoptimized(nodes, config)
 	}
 }
