747840864f0a916a56acf7a4da81286b9ca387fe
Chris Trevino <darthtrevino@gmail.com>
2019-12-18 13:46:28 -0800

    switch gravity to node-proxies

--- src/core/forces/computeGravity.ts
+++ src/core/forces/computeGravity.ts
@@ -1,42 +1,32 @@
-import { NodeStore, ppn } from '../marshaling'
+import { Node, NodeStore } from '../marshaling'
 import { FA2Configuration } from '../../configuration'
 
 export function computeGravity(nodes: NodeStore, config: FA2Configuration) {
-	for (let n = 0; n < nodes.bufferLength; n += ppn) {
-		// Common to both methods
-		const xDist = nodes.x(n)
-		const yDist = nodes.y(n)
-		const distance = Math.sqrt(xDist ** 2 + yDist ** 2)
-		const factor = getGravityFactor(nodes, n, distance, config)
+	const g = config.gravity / config.scalingRatio
+	const coefficient = config.scalingRatio
+	let node: Node
+	let distance: number
+	let factor: number
+
+	for (let i = 0; i < nodes.nodeCount; ++i) {
+		node = nodes.getNode(i)
+		distance = Math.sqrt(node.x ** 2 + node.y ** 2)
+
+		let factor = 0
+		if (config.strongGravityMode) {
+			// strong gravity
+			if (distance > 0) {
+				factor = coefficient * node.mass * g
+			}
+		} else {
+			// linear anti-collision repulsion
+			if (distance > 0) {
+				factor = (coefficient * node.mass * g) / distance
+			}
+		}
 
 		// Updating node's dx and dy
-		nodes.subDx(n, xDist * factor)
-		nodes.subDy(n, yDist * factor)
+		node.dx -= node.x * factor
+		node.dy -= node.y * factor
 	}
-}
-
-function getGravityFactor(
-	nodes: NodeStore,
-	n: number,
-	distance: number,
-	config: FA2Configuration,
-) {
-	const scaledGravity = config.gravity / config.scalingRatio
-
-	const coefficient = config.scalingRatio
-	const g = scaledGravity
-
-	let factor = 0
-	if (config.strongGravityMode) {
-		// strong gravity
-		if (distance > 0) {
-			factor = coefficient * nodes.mass(n) * g
-		}
-	} else {
-		// linear anti-collision repulsion
-		if (distance > 0) {
-			factor = (coefficient * nodes.mass(n) * g) / distance
-		}
-	}
-	return factor
 }
