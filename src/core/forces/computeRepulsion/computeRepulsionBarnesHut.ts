8b682a32ac11f3fcc79a393178d236cdb8bb4fe8
Chris Trevino <darthtrevino@gmail.com>
2019-12-18 14:00:25 -0800

    use node/edge readr in repulsion calc

--- src/core/forces/computeRepulsion/computeRepulsionBarnesHut.ts
+++ src/core/forces/computeRepulsion/computeRepulsionBarnesHut.ts
@@ -1,4 +1,4 @@
-import { NodeStore, ppn, QuadTree } from '../../marshaling'
+import { Node, NodeStore, QuadTree } from '../../marshaling'
 import { FA2Configuration } from '../../../configuration'
 import { computeNodeRepulsion } from './computeNodeRepulsion'
 
@@ -7,39 +7,38 @@ export function computeRepulsionBarnesHut(
 	qt: QuadTree,
 	config: FA2Configuration,
 ) {
-	for (let n1 = 0; n1 < nodes.bufferLength; n1 += ppn) {
-		applyQuadTreeRepulsion(qt, nodes, n1, config)
+	for (let n = 0; n < nodes.nodeCount; n++) {
+		applyQuadTreeRepulsion(qt, nodes.getNode(n), config)
 	}
 }
 
 function applyQuadTreeRepulsion(
 	root: QuadTree,
-	nodes: NodeStore,
-	n1: number,
+	n1: Node,
 	config: FA2Configuration,
 ) {
 	root.visit(qt => {
 		if (qt.isLeaf) {
 			if (qt.node) {
-				computeNodeRepulsion(config, nodes, n1, qt.node.index)
+				computeNodeRepulsion(n1, qt.node, config)
 			}
 			return true
 		}
-		const xDist = nodes.x(n1) - qt.centerOfMassX
-		const yDist = nodes.y(n1) - qt.centerOfMassY
+		const xDist = n1.x - qt.centerOfMassX
+		const yDist = n1.y - qt.centerOfMassY
 		const distance = Math.sqrt(xDist ** 2 + yDist ** 2)
 		const applyQuadForce = qt.size / distance < config.barnesHutTheta
 
 		if (applyQuadForce) {
 			const coefficient = config.scalingRatio
-			const massCoeff = coefficient * nodes.mass(n1) * qt.mass
+			const massCoeff = coefficient * n1.mass * qt.mass
 
 			//-- Linear Repulsion
 			if (distance > 0) {
 				// Updating nodes' dx and dy
 				const factor = massCoeff / distance ** 2
-				nodes.addDx(n1, xDist * factor)
-				nodes.addDy(n1, yDist * factor)
+				n1.dx += xDist * factor
+				n1.dy += yDist * factor
 			} else {
 				console.log('Zero Distance 3')
 			}
